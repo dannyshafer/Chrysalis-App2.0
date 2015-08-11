7f5b55e6c7d60a21cea067250cd9ee090275c644
ryanau <ryan.au@hotmail.com>
2015-08-11 08:27:20 -0700

    add routes for basket

--- client/components/UserBaskets.jsx
+++ client/components/UserBaskets.jsx
@@ -1,6 +1,39 @@
 var React = require('react');
 
+var mui = require('material-ui');
+var ThemeManager = new mui.Styles.ThemeManager();
+
 var UserBaskets = React.createClass({
+	getInitialState: function () {
+		return {
+
+		};
+	},
+
+	childContextTypes: {
+		muiTheme: React.PropTypes.object
+	},
+
+	getChildContext: function () { 
+		return {
+			muiTheme: ThemeManager.getCurrentTheme()
+		};
+	},
+
+	componentDidMount: function () {
+
+	},
+
+	readUserBasketsFromAPI: function () {
+		var uid = this.props.currentUser.uid
+		this.props.readFromAPI(this.props.origin + '/users/' + uid + '/baskets', function(info){
+		  this.setState({
+
+
+		  });
+		}.bind(this));
+	},
+
 	render: function () {
 		return (
 			<div>
