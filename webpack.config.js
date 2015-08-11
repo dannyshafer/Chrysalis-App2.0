1622fec2f2072f7a2a992e79ab0c51274717510e
alookatommorow <hessjohnt@gmail.com>
2015-08-11 12:44:07 -0700

    added resolveLoader to webpack config to try and fix packing on heroku

--- webpack.config.js
+++ webpack.config.js
@@ -1,8 +1,13 @@
+var path = require('path');
+
 module.exports = {
   entry: ['./client/main.jsx'],
   output: {
     path: './public',
     filename: 'bundle.js'
+  },
+  resolveLoader: {
+    root: path.join(__dirname, 'node_modules')
   },
   module: {
     loaders: [
