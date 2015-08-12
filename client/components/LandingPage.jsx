5a5279361e6131ca86fa0e3fb7497bba9a8d23a0
[01;31m[K"name"[m[K: "Andrew Donato <[01;31m[K"email"[m[K: "andrew.donato91@gmail.com>
2015-08-12 11:23:41 -0700

    working on landing css containers

--- client/components/LandingPage.jsx
+++ client/components/LandingPage.jsx
@@ -45,18 +45,50 @@ var LandingPage = React.createClass({
       );
     };
     return (
-      <div>
-        {profileSetUpModal}
-        <div className="slogan">
-          <h1 className='title'>Chrysalis</h1>
-          <h2 className="sub-title">hatching insight</h2>
-        </div>
-        <div className='video-container'>
-          <video preload="true" autoPlay="autoplay" loop="loop" muted tabIndex="0">
-            <source src="ChrysalisRain.mp4" type="video/mp4"></source>
-          </video>
-        </div>
+      <div className="landing-container">
+        <span>
+          {profileSetUpModal}
+          <div className="slogan">
+            <h1 className='title'>Chrysalis</h1>
+            <h2 className="sub-title">hatching insight</h2>
+          </div>
 
+                  <div className="row">
+                    <div className='outer-video-container'>
+                      <div className='video-container'>
+                        <div className="small-12 columns" >
+                          <video preload="true" autoPlay="autoplay" loop="loop" muted tabIndex="0">
+                            <source src="low.mp4" type="video/mp4"></source>
+                          </video>
+                        </div>
+                      </div>
+                    </div>
+                  </div>
+
+
+                  <div className="outer-mission-statement-container">
+                    <div className="row">
+                      <div className="small-12 columns">
+                        <div className="mission-statement-container">
+                          <h6>Chrysalis</h6>
+                          <p>
+                            "Chrysalis is a fundamental analysis website that seeks suitable investments based loosely on stock valuation techniques by Benjamin Graham. Other services (particularly brokers) which take age and risk preference typically only return a suggested proportional mix of asset classes so that the user it dependent on their service to do the actual management of buying and selling. Chrysalis is dramatically more actionable as it suggests a mix but also offers specific securities which the user can then research further and buy through his/her preferred channel.
+
+                            Stock picks are filtered from the universe of all US stocks based on the following qualifications:
+
+                            PE ratio lower than the industry average.
+                            EPS ratio higher than the industry average.
+                            PED ratio lower than the industry average.
+                            Graham Number higher than the current price.
+
+                            While any of these constraints could be met by a given stock, fewer than five percent qualify for all four."
+                          </p>
+                      </div>
+                    </div>
+                  </div>
+                </div>
+
+        </span>
       </div>
     );
   },
