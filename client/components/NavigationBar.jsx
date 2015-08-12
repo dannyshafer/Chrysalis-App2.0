70551431436943b3f8b74b218a4390581411abdc
[01;31m[K"name"[m[K: "Andrew Donato <[01;31m[K"email"[m[K: "andrew.donato91@gmail.com>
2015-08-12 16:03:52 -0700

    add new navbar, still not collapsible

---
+++ client/components/NavigationBar.jsx
@@ -0,0 +1,52 @@
+var React = require('react');
+var Router = require('react-router');
+var ReactBootstrap = require('react-bootstrap');
+var ReactRouterBootstrap = require('react-router-bootstrap');
+var Link = Router.Link;
+var Nav = ReactBootstrap.Nav;
+var Navbar = ReactBootstrap.Navbar;
+var CollapsibleNav = ReactBootstrap.CollapsibleNav;
+var Button = ReactBootstrap.Button;
+var NavItem = ReactBootstrap.NavItem;
+var NavItemLink = ReactRouterBootstrap.NavItemLink;
+
+
+var NavigationBar = React.createClass({
+  handleSignOutLink: function() {
+    sessionStorage.setItem('jwt','');
+    location = '/';
+  },
+
+  render: function(){
+
+    if (this.props.signedIn) {
+      var profileLink = <NavItemLink className="nav-item" to="profile">{this.props.currentUser}</NavItemLink>
+      var recommendationLink = <NavItemLink className="nav-item" to="recommendation">Recommendations</NavItemLink>
+      var updateLink = <NavItemLink className="nav-item" to="update">Update Stocks</NavItemLink>
+      var userBasketLink = <NavItemLink className="nav-item" to="user_baskets">Baskets</NavItemLink>
+      var signingLink = <span className="nav-item" onClick={this.handleSignOutLink}>Sign Out</span>
+      var glossary = <NavItemLink className="nav-item" to="glossary"><i className="material-icons">description</i></NavItemLink>
+    } else {
+      var signingLink = <a className="nav-item" href={this.props.origin + '/request_token'}>Sign In</a>;
+    }
+    return (
+      <div>
+        <Navbar className="menubar" fixedTop>
+          <CollapsibleNav>
+          <Nav>
+            <NavItemLink className="nav-item" to="landingpage" >Chrysalis</NavItemLink>
+            {profileLink}
+            {recommendationLink}
+            {userBasketLink}
+            {updateLink}
+            {signingLink}
+            {glossary}
+          </Nav>
+          </CollapsibleNav>
+        </Navbar>
+      </div>
+      );
+  },
+});
+
+module.exports = NavigationBar;
